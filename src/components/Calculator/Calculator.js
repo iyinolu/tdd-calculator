5af87ddbd73bb0c7a62d5b87b4b468ee064e3844
Paul Osinowo <paulosinowo@Pauls-MacBook-Pro.local>
2022-07-23 12:33:21 +0100

    Fix failing test cases

--- src/components/Calculator/Calculator.js
+++ src/components/Calculator/Calculator.js
@@ -1,43 +1,88 @@
+/* eslint-disable no-eval */
 import React, { Component } from "react";
 import Display from "../Display/Display";
-import "../../styles/Calculator.css"
+import "../../styles/Calculator.css";
 import Keypad from "../Keypad/Keypad";
 
 class Calculator extends Component {
   state = {
     displayValue: "0",
-    numbers: ['9', '8', '7', '6', '5', '4', '3', '2', '1', '.', '0','ce'],
-    operators: ['/', 'x', '-', '+'],
+    numbers: ["9", "8", "7", "6", "5", "4", "3", "2", "1", ".", "0", "ce"],
+    operators: ["/", "x", "-", "+"],
     selectedOperator: "",
     storedValue: "",
   };
 
   callOperator = () => {
-    console.log("call operation");
+    let result;
+    let { storedValue, selectedOperator, displayValue } = this.state;
+    storedValue = parseFloat(storedValue);
+    displayValue = parseFloat(displayValue);
+    switch (selectedOperator) {
+      case "+":
+        result = storedValue + displayValue;
+        break;
+      case "-":
+        result = storedValue - displayValue;
+        break;
+      case "/":
+        result = storedValue / displayValue;
+        break;
+      case "x":
+        result = storedValue * displayValue;
+        break;
+      default:
+        result = 0;
+    }
+    if (result === Infinity || isNaN(result)) {
+      result = 0;
+    }
+    this.setState({ displayValue: String(result) });
   };
 
-  setOperator = () => {
-    console.log("set operation");
+  setOperator = (value) => {
+    let { selectedOperator, storedValue, displayValue } = this.state;
+    selectedOperator = value;
+    storedValue = displayValue !== "0" ? this.state.displayValue : storedValue;
+    displayValue = "0";
+
+    this.setState({ selectedOperator, storedValue, displayValue });
   };
 
-  updateDisplay = () => {
-    console.log("update display");
+  updateDisplay = (value) => {
+    let { displayValue } = this.state;
+    if (value === "." && displayValue.includes(".")) {
+      value = "";
+    }
+    if (value === "ce") {
+      displayValue = displayValue.slice(0, -1);
+    } else if (displayValue.length > 0) {
+      displayValue = displayValue + value;
+    }
+    if (displayValue[0] === "0") {
+      displayValue = displayValue.slice(1);
+    }
+    if (displayValue === "") {
+      displayValue = "0";
+    }
+
+    this.setState({ displayValue });
   };
 
   render = () => {
-      return (
-          <div className="calculator-container">
-              <Display />
-              <Keypad 
-                callOperator={this.callOperator}
-                numbers={this.state.numbers}
-                operators={this.state.operators}
-                setOperator={this.setOperator}
-                updateDisplay={this.updateDisplay}
-              />
-          </div>
-      )
-  }
+    return (
+      <div className="calculator-container">
+        <Display displayValue={this.state.displayValue} />
+        <Keypad
+          callOperator={this.callOperator}
+          numbers={this.state.numbers}
+          operators={this.state.operators}
+          setOperator={this.setOperator}
+          updateDisplay={this.updateDisplay}
+        />
+      </div>
+    );
+  };
 }
 
 export default Calculator;
